20f2edb1f938c231e076de35b8051e84ebad42ee
Ivan Scherbakov <ivashka1113@mail.ru>
2022-09-29 23:24:04 +0700

    Создал переменные и вывел сообщения

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = "Lesson_1";
+let screens = 2;
+let screenPrice = 1200;
+const rollback = 24;
+let fullPrice = 1999;
+let adaptive = 14;
+alert("Hello world");
+console.log("Hello world");
