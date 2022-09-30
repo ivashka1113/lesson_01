e53330fe940d9cae7088528315da39a52ca38c23
Ivan Scherbakov <ivashka1113@mail.ru>
2022-09-30 18:44:51 +0700

    Выполнил домашнее задание второго урока

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const title = "Lesson_1";
-let screens = 2;
-let screenPrice = 1200;
-const rollback = 24;
-let fullPrice = 1999;
-let adaptive = 14;
-alert("Hello world");
-console.log("Hello world");
+const title = "Lesson_2";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 12000;
+let rollback = 24;
+let fullPrice = 25000;
+const adaptive = false;
+
+console.log(" Тип данных переменной title: " + typeof title + "\n", "Тип данных переменной fullPrice: " + typeof fullPrice + "\n", "Тип данных переменной adaptive: " + typeof adaptive);
+console.log(" Длина строки из переменной screens: " + screens.length);
+console.log(" Стоимость верстки экранов: " + screenPrice + " рублей\n Стоимость разработки сайта: " + fullPrice + " рублей");
+console.log((screens.toLowerCase()).split(", "));
+console.log("Процент отката посреднику за работу: " + fullPrice * (rollback / 100) + " рублей");
