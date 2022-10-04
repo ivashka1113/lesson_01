2c2295e45b1d462426d36aa7fa1934c29c97adce
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-04 21:43:45 +0700

    Выполнил домашнее задание 4

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 let rollback = 24;
 
-let title = prompt("Как называется ваш проект?", "АвтоЛидер");
+
 let screens = prompt("Какие типы экранов нужно разработать?", 'Простые, Сложные, Интерактивные');
 let screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
 let adaptive = confirm("Нужен ли адаптив на сайте? (Ок - да, отмена - нет)");
-
 let service1 = prompt("Какой дополнительный тип услуги нужен?", "SEO продвижение");
 let servicePrice1 = +prompt("Сколько это будет стоить?", 6000);
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "SEO продвижениеу");
 let servicePrice2 = +prompt("Сколько это будет стоить?", 6000);
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
 
 
 const showTypeOf = function (variable) {
@@ -34,16 +30,38 @@ const getRollbackMessage = function (price) {
     }
 }
 
+const getAllServicePrices = function (servPrice1, servPrice2) {
+    return servPrice1 + servPrice2;
+}
+
+function getFullPrice(price, servPrices) {
+    return price + servPrices;
+}
+
+const getTitle = function (name) {
+    let nameTrans = name
+        .trim()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+    return nameTrans;
+}
+
+const getServicePercentPrices = function (price, roll) {
+    return price - price * (roll / 100);
+}
+
+
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+let fullPrice = getFullPrice(screenPrice, allServicePrices);
+let title = getTitle(prompt("Как называется ваш проект?", " авто Лидер"));
+let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
 
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
 
-
-console.log(" Тип данных переменной title: " + typeof title + "\n", "Тип данных переменной fullPrice: " + typeof fullPrice + "\n", "Тип данных переменной adaptive: " + typeof adaptive);
-console.log(" Длина строки из переменной screens: " + screens.length);
-console.log(" Стоимость верстки экранов: " + screenPrice + " рублей\n Стоимость разработки сайта: " + fullPrice + " рублей");
 console.log((screens.toLowerCase()).split(", "));
-console.log("Процент отката посреднику за работу: " + fullPrice * (rollback / 100) + " рублей");
-console.log("Итоговая стоимость за вычетом отката посреднику: " + Math.ceil(servicePercentPrice) + " рублей");
 console.log(getRollbackMessage(fullPrice));
+console.log("Итоговая стоимость за вычетом отката посреднику: " + Math.ceil(servicePercentPrice) + " рублей");
